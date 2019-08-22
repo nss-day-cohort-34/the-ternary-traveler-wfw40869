ac5ffdae988672945f499c265c512e6f6b453ce8
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-08-22 11:55:19 -0500

    Finished project, and most styling

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -5,6 +5,10 @@ const API = {
         return fetch("http://localhost:8088/interests?_expand=place")
             .then(response => response.json())
     },
+    getInterestById: (interestId) => {
+        return fetch(`http://localhost:8088/interests/${interestId}?_expand=place`)
+            .then(response => response.json())
+    },
     addInterest: (newInterest) => {
         return fetch("http://localhost:8088/interests", {
             method: "POST",
@@ -15,6 +19,22 @@ const API = {
         })
             .then(response => response.json())
 
+    },
+    deleteInterest: (interestId) => {
+        return fetch(`http://localhost:8088/interests/${interestId}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json())
+    },
+    editInterest: (interestId, updatedInterest) => {
+        return fetch(`http://localhost:8088/interests/${interestId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedInterest)
+        })
+            .then(entry => entry.json())
     }
 
 }
--- src/scripts/events.js
+++ src/scripts/events.js
@@ -36,6 +36,54 @@ const registerListeners = () => {
             // Clears and closes modal
             modal.innerHTML = ""
             modal.close()
+            // SHOW MODAL IF SOMEONE CLICKS DELETE ON A CARD
+        } else if (event.target.id.startsWith("deleteInterest")) {
+            const interestId = event.target.id.split("--")[1]
+            modal.innerHTML = factoryObj.createDeletePromptHTML(interestId)
+            modal.showModal()
+            // DELETE THE ENTRY IF THE USER CLICKS YES
+        } else if (event.target.id.startsWith("confirmDelete")) {
+            const interestId = event.target.id.split("--")[1]
+            API.deleteInterest(interestId)
+                .then(API.getInterests)
+                .then(renderInterests)
+            modal.innerHTML = ""
+            modal.close()
+            // OTHERWISE CLOSE THE MODAL AND DON'T DELETE IT
+        } else if (event.target.id.startsWith("denyDelete")) {
+            modal.innerHTML = ""
+            modal.close()
+            //  EDIT FUNCTIONALITY STARTS HERE
+        } else if (event.target.id.startsWith("editInterest")) {
+            const interestId = event.target.id.split("--")[1]
+            API.getInterestById(interestId)
+                .then(parsedInterest => {
+                    modal.innerHTML = factoryObj.createEditPrompt(parsedInterest)
+                    modal.showModal()
+                })
+        } else if (event.target.id.startsWith("confirmEdit")) {
+            const interestId = event.target.id.split("--")[1],
+                placeSelect = document.querySelector("#interestLocation__select"),
+                nameInput = document.querySelector("#interestName__input"),
+                reviewInput = document.querySelector("#interestReview__input"),
+                costInput = document.querySelector("#interestCost__input"),
+                descriptionInput = document.querySelector("#interestDescription__textarea")
+
+            const editedInterest = {
+                placeId: placeSelect.value,
+                name: nameInput.value,
+                review: reviewInput.value,
+                cost: costInput.value,
+                description: descriptionInput.value
+            }
+            API.editInterest(interestId, editedInterest)
+                .then(API.getInterests)
+                .then(parsedInterests => {
+                    console.log(parsedInterests)
+                    modal.innerHTML = ""
+                    modal.close()
+                    renderInterests(parsedInterests)
+                })
         }
 
     })
